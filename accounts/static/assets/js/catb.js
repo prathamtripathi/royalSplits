a402f970898da9cb2408ec8d8cec4a71154ef3f9
pratham0203 <prathamtripathi011@gmail.com>
2021-07-08 13:16:53 +0530

    accounts

---
+++ accounts/static/assets/js/catb.js
@@ -0,0 +1,106 @@
+
+const myFunction30 = () => {
+    document.getElementById("first11").style.display ='block';
+    document.getElementById("second12").style.display ='none'
+    document.getElementById("third13").style.display ='none'
+    document.getElementById("mainphoto3").style.display ='none'
+  }
+  
+  const myFunction31 = () => {
+    document.getElementById("second12").style.display ='block'
+    document.getElementById("first11").style.display ='none'
+    document.getElementById("third13").style.display ='none'
+    document.getElementById("mainphoto3").style.display ='none'
+  }
+  
+  const myFunction32 = () => {
+    document.getElementById("mainphoto3").style.display ='none'
+    document.getElementById("third13").style.display ='block'
+    document.getElementById("first11").style.display ='none'
+    document.getElementById("second12").style.display ='none'
+    
+  }
+  const myFunction301 = () => {
+    document.getElementById("mainphoto5").style.display ='none';
+    document.getElementById("first31").style.display ='block'
+  }
+
+  const myFunction330 = () => {
+    document.getElementById("mainphoto4").style.display ='none';
+    document.getElementById("first331").style.display ='block';
+    document.getElementById("second332").style.display ='none'
+    document.getElementById("third333").style.display ='none'
+    document.getElementById("fourth334").style.display ='none'
+    document.getElementById("five335").style.display ='none'
+    document.getElementById("six336").style.display ='none'
+    document.getElementById("sev337").style.display ='none'
+  }
+  
+  const myFunction331 = () => {
+    document.getElementById("mainphoto4").style.display ='none';
+    document.getElementById("second332").style.display ='block'
+    document.getElementById("first331").style.display ='none'
+    document.getElementById("third333").style.display ='none'
+    document.getElementById("fourth334").style.display ='none'
+    document.getElementById("five335").style.display ='none'
+    document.getElementById("six336").style.display ='none'
+    document.getElementById("sev337").style.display ='none'
+  }
+  
+  const myFunction332 = () => {
+    document.getElementById("mainphoto4").style.display ='none';
+    document.getElementById("third333").style.display ='block'
+    document.getElementById("first331").style.display ='none'
+    document.getElementById("second332").style.display ='none'
+    document.getElementById("fourth334").style.display ='none'
+    document.getElementById("six336").style.display ='none'
+    document.getElementById("five335").style.display ='none'
+    document.getElementById("sev337").style.display ='none'
+    
+  }
+  const myFunction333 = () => {
+    document.getElementById("mainphoto4").style.display ='none';
+    document.getElementById("third333").style.display ='none'
+    document.getElementById("first331").style.display ='none'
+    document.getElementById("second332").style.display ='none'
+    document.getElementById("fourth334").style.display ='block'
+    document.getElementById("five335").style.display ='none'
+    document.getElementById("six336").style.display ='none'
+    document.getElementById("sev337").style.display ='none'
+    
+  }
+  const myFunction334 = () => {
+    document.getElementById("mainphoto4").style.display ='none';
+    document.getElementById("third333").style.display ='none'
+    document.getElementById("first331").style.display ='none'
+    document.getElementById("second332").style.display ='none'
+    document.getElementById("fourth334").style.display ='none'
+    document.getElementById("five335").style.display ='block'
+    document.getElementById("six336").style.display ='none'
+    document.getElementById("sev337").style.display ='none'
+    
+  }
+  const myFunction335 = () => {
+    document.getElementById("mainphoto4").style.display ='none';
+    document.getElementById("third333").style.display ='none'
+    document.getElementById("first331").style.display ='none'
+    document.getElementById("second332").style.display ='none'
+    document.getElementById("fourth334").style.display ='none'
+    document.getElementById("five335").style.display ='none'
+    document.getElementById("six336").style.display ='block'
+    document.getElementById("sev337").style.display ='none'
+    
+  }
+  const myFunction336 = () => {
+    document.getElementById("mainphoto4").style.display ='none';
+    document.getElementById("third333").style.display ='none'
+    document.getElementById("first331").style.display ='none'
+    document.getElementById("second332").style.display ='none'
+    document.getElementById("fourth334").style.display ='none'
+    document.getElementById("five335").style.display ='none'
+    document.getElementById("six336").style.display ='none'
+    document.getElementById("sev337").style.display ='block'
+    
+  }
+  
+  
---
+++ accounts/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path,include
+from . import views
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    path('signup', views.signup, name='signup'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+    path('social-accounts/', include('allauth.urls')),
+
+     path('reset_password/',
+     auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"),
+     name="reset_password"),
+
+    path('reset_password_sent/', 
+        auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"), 
+        name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+     auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html"), 
+     name="password_reset_confirm"),
+
+    path('reset_password_complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_done.html"), 
+        name="password_reset_complete"),
+
+]
